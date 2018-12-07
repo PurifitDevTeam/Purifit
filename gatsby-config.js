111686913c123cc019536f648c07fe886dbdfe6f
Jishnu Koottala <jishnu61@gmail.com>
2018-12-07 22:46:29 +0530

    removed unused plugins in gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,31 +25,7 @@ module.exports = {
       },
     },
        'gatsby-plugin-offline',
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: `${__dirname}/src/posts/`
-      }
-    },
-    
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: "UA-128168119-1",
-        // Puts tracking script in the head instead of the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        
-      },
-    },
-    "gatsby-transformer-remark"
+       'gatsby-transformer-remark'
     
   ],
 }
