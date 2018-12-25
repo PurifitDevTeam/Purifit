45e73da6ab966a8d7cf021130a9f985dea968831
Jishnu Koottala <jishnu61@gmail.com>
2018-12-25 11:59:53 +0530

    dynamic data

--- src/pages/index.js
+++ src/pages/index.js
@@ -117,7 +117,7 @@ console.log('######## data is ',data);
   return ( 
     <React.Fragment>
     <SiteMeta/>
-    <IndexComponent  DoctorSectionData={data.allContentfulDoctorReccomendations.edges} WhatWhySection={data.allContentfulWhatWhySection.edges} LandingViewContent={data.allContentfulLandingViewContent.edges}  numbersSectionData={data.allContentfulNumbersSection.edges}/>
+    <IndexComponent TestimonialsData={data.allContentfulTestimonials.edges} ProductFeaturesData={data.allContentfulProductFeatures.edges} DoctorSectionData={data.allContentfulDoctorReccomendations.edges} WhatWhySection={data.allContentfulWhatWhySection.edges} LandingViewContent={data.allContentfulLandingViewContent.edges}  numbersSectionData={data.allContentfulNumbersSection.edges}/>
     </React.Fragment>
   )
 }
@@ -189,6 +189,11 @@ export const query = graphql`
             }
             customerName
             customerPlace
+            image {
+              file {
+                url
+              }
+            }
           }
         }
       }
@@ -225,6 +230,9 @@ export const query = graphql`
             }
             productImage {
               id
+              file{
+                url
+              }
               
             }
             
